83bec49b344ef80b55ad8c9699514dcf2eacdadd
jtaren <arenjatau@gmail.com>
2025-02-01 21:11:01 +0100

    decorators

--- WPPJ/decorators.py
+++ WPPJ/decorators.py
@@ -1,1 +1,10 @@
+def announce(f):
+    def wrapper():
+        pprint("About to run the function...")
+        f()
+        print("Done with the function")
+    return wrapper
 
+@announce
+def hello():
+    print("Hello, world!")
