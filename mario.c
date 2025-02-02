380408a68ede89a43480eb8d25be75b77f51ecec
Solomon Jt Aren <arenjatau@gmail.com>
2025-02-02 23:47:52 +0100

    Update mario.c

---
+++ mario.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+  // Prompt user for input
+  int height = get_int("What is the height of the pyramid?");
+
+  // Print a pyramid of that height
+}
